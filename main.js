a5c4900738b8ce49be50e20419be9beee69d3fcd
Jordan Michalak <jordan.michalak17@gmail.com>
2021-01-16 08:04:51 +0100

    add new version

--- main.js
+++ main.js
@@ -2,111 +2,120 @@
 // Jordan Michalak
 // s449340
 
+//Działanie algorytmu: znajdz największą liczbę i zamień ją z sąsiadem na którego patrzy kierunek < 
+
+
 const btn1 = document.querySelector('button:nth-of-type(1)');
 const btn2 = document.querySelector('button:nth-of-type(2)');
 const dataIn = document.querySelector('#number');
 const result = document.querySelector('.result');
 const footer = document.querySelector('footer');
   
-let toRight = true; 
-let toLeft = false; 
+const toRight = 1; 
+const toLeft = 0; 
 let resultBuffor = [];
 
+// Obliczanie silni
+const factorialize = (num) => {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorialize(num - 1));
+    }
+  }
 
-function printPermutation(n) { 
+const showPermutation = (n) => { 
     let a = [n]; 
     let dir = [n]; 
-  
+    
+    //Wypisanie pierwszej permutacji
     for (let i = 0; i < n; i++) { 
         a[i] = i + 1; 
-        console.log(a[i]);
-        resultBuffor[i] = a[i]; 
+        resultBuffor[i] = a[i];    // 1,2,3,4
     }
 
     resultBuffor.push(' ');
-     
+     //Ustawienie dla każdej cyfry od 1 do n kierunku w lewą stronę  < 1 < 2 < 3 < 4
     for (let i = 0; i < n; i++) 
         dir[i] =  toLeft; 
   
-    for (let i = 1; i < fact(n); i++) 
-        printOnePerm(a, dir, n);
-     
+    //Generowanie pojedyńczych permutacji n! razy      
+    for (let i = 1; i < factorialize(n); i++) 
+        showSinglePermutation(a, dir, n);    
+
     result.innerHTML = `${resultBuffor}`;
 } 
 
-function printOnePerm(a, dir, n) {
+//Pokaz pojedyncza permutacje
+const showSinglePermutation = (a, dir, n) => {
 
-    let max = getMaxNumber(a, dir, n); 
-    let pos = searchArr(a, n, max); 
+    let max = getMaxNumber(a, dir, n);  // -> 4 
+    let maxPos = findMaxPos(a, n, max); // -> 4
   
-    if (dir[a[pos - 1] - 1] ==  toLeft){
+    //Jeżeli kierunek jest na LEWO zamiana dwóch sąsiadów a b -> b a
+    if (dir[a[maxPos - 1] - 1] ==  toLeft){
         let b; 
-        b = a[pos-2];
-        a[pos-2] = a[pos-1];
-        a[pos-1] = b;
+        b = a[maxPos-2]; // -> 3
+        a[maxPos-2] = a[maxPos-1]; // 3 = 4
+        a[maxPos-1] = b;   // 4 = 3
     }
-  
-    else if (dir[a[pos - 1] - 1] == toRight){
+   
+    //Jezeli kierunek jest ustawiony na PRAWO
+    else if (dir[a[maxPos - 1] - 1] == toRight){
         let b; 
-        a[pos], a[pos-1]; 
-        b = a[pos-1];
-        a[pos-1] = a[pos];
-        a[pos] = b;
+        a[maxPos], a[maxPos-1]; // -> 4 , 3
+        b = a[maxPos-1];  // -> 3
+        a[maxPos-1] = a[maxPos]; // 3 = 4
+        a[maxPos] = b;   // 4 = 3 
     }
   
     for (let i = 0; i < n; i++) { 
-        if (a[i] > max) { 
-            if (dir[a[i] - 1] == toRight) 
-                dir[a[i] - 1] = toLeft; 
+        if (a[i] > max) { // 4
+            if (dir[a[i] - 1] == toRight) // 3 
+                dir[a[i] - 1] = toLeft; // zmiana z PRAWO na LEWO np 1 > 2 > 3 > 4 - 1 > 2 > 3 < 4
             else if (dir[a[i] - 1] == toLeft) 
                 dir[a[i] - 1] = toRight; 
         } 
     } 
 
     for (let i = 0; i < n; i++){
-        console.log(a[i]);
         resultBuffor.push(a[i]);
     }
     resultBuffor.push(` `);
-    console.log(" ");
+
 } 
 
-function getMaxNumber(a, dir, n) {
-
-    let maxPrev = 0, max = 0; 
-    for (let i = 0; i < n; i++) { 
+// Szukanie największej liczby
+const getMaxNumber = (a, dir, n) => {
+    let max = 0; 
+    for (let i = 0; i < n; i++) {
+        //Jżeli poprzedni kierunek ustawiony jest na LEWO  to jeżeli aktualna licba jest większa od poprzedniej to teraz ta liczba jest największa 
         if (dir[a[i]-1] == toLeft && i!=0) { 
-            if (a[i] > a[i-1] && a[i] > maxPrev) { 
+            if (a[i] > a[i-1] && a[i] > max) { 
                 max = a[i]; 
-                maxPrev = max; 
             } 
         } 
-  
+        
+        //Jeżeli poprzedni kierunek ustawiony jest na PRAWO to jeśli aktualna liczba jest większa od następnej to teraz aktualna liczba jest największą
         if (dir[a[i]-1] == toRight && i!=n-1) { 
-            if (a[i] > a[i+1] && a[i] > maxPrev) { 
+            if (a[i] > a[i+1] && a[i] > max) { 
                 max = a[i]; 
-                maxPrev = max; 
             } 
         } 
-    } 
-  
-    if (max == 0 && maxPrev == 0) 
+    }
+     
+    if (max == 0) 
         return 0; 
     else
         return max; 
 } 
 
-function searchArr(a,  n, max) { 
+const findMaxPos = (a,  n, max) => { 
     for (let i = 0; i < n; i++) 
         if (a[i] == max) 
            return i + 1; 
-} 
-
-function fact(n) { 
-    let res = 1; 
-    for (let i = 1; i <= n; i++) 
-        res = res * i; 
-    return res; 
 } 
   
   
@@ -116,7 +125,7 @@ btn1.addEventListener('click', function(){
             result.innerHTML = 'Aby pokazywany wynik był prawidłowy najpierw wsciśnij "WYCZYŚĆ" i spróbuj ponownie'
         }
         else{
-        printPermutation(dataIn.value);
+        showPermutation(dataIn.value);
         }
     }
     else{
